464e31d6d53de7ab8ab05ddd11ff8b12e7e5e04f
Ha989 <viethanguyen0809@gmail.com>
2022-12-25 09:30:50 +0700

    add timer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import "Your code here";
+import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+  const { time, startTimer, stopTimer, resetTimer, lapTimer, clearLaps,laps, active } = useTimer(0);
 
   return (
     <div className="App container">
+      <div className="timer">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{"Your code here"}</p>
+          <p>{formatTime(time)}</p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={lapTimer}>
+            Lap
+          </button>
+  
+          <button className="button" onClick={stopTimer}>
             Stop
           </button>
           <button
             className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
+            ref={active}
+            onClick={startTimer}
           >
             Start
           </button>
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={resetTimer}>
             Reset
           </button>
+          <button className="button" onClick={clearLaps}>
+            Clear Laps
+          </button>
         </div>
+        </div>
+        </div>
+      <div>
+      {laps.map((lap, index) => (
+      <div className="lap-list" key={index + 1} >
+      <p >Lap     {index + 1}: {formatTime(lap)}</p>
+      </div>))
+      }
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,42 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [time, setTime] = useState(0);
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(false);
+  const active = useRef();
+  const refInterval = useRef(null);
+  const [laps, setLaps] = useState([]);
 
   const startTimer = () => {
-    "Your code here";
+    isStart.current = true;
     active.current.disabled = true;
+    refInterval.current = setInterval(() => {
+        setTime((time) => time + 1);
+    }, 1000)
   };
+
   const stopTimer = () => {
-    "Your code here";
+    isStart.current = true;
+    active.current.disabled = false;
+    clearInterval(refInterval.current);
   };
+  
   const resetTimer = () => {
-    "Your code here";
+    setTime(0);
+    // setLaps([]);
+    clearInterval(refInterval.current);
     active.current.disabled = false;
   };
 
-  return { time, startTimer, stopTimer, resetTimer, active };
+  const lapTimer = () => {
+    setLaps((prevLap) => [...prevLap, time])
+  };
+
+  const clearLaps = () => {
+    setLaps([]);
+  }
+
+  return { time, startTimer, stopTimer, resetTimer, laps, lapTimer,clearLaps, active };
 };
 export default useTimer;
